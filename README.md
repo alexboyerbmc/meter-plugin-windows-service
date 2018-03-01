e2afb6276a2583faa4d29033d996d8cbcb03131d
David Cox <dcox@bmc.com>
2018-03-01 13:33:05 -0600

    Initial version

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# win_service_test
+# win_service_monitor
+=======
+# TrueSight Pulse Windows Service Monitor Plugin
+
+The TrueSight Pulse Windows Service Monitor provides a plugin whereby users can monitor service state, and perform
+optional actions via PowerShell scripts when a service is down.
+
+### Prerequisites
+
+#### Supported OS
+
+|     OS    | Linux | Windows | OS X |
+|:----------|:-----:|:-------:|:----:|
+| Supported |   -   |    v    |  -   |
+
+#### TrueSight Pulse Meter versions v4.8.0 or later
+
+- To install new meter go to Settings->Installation or [see instructions](https://help.truesight.bmc.com/hc/en-us/sections/200634331-Installation).
+- To upgrade the meter to the latest version - [see instructions](https://help.truesight.bmc.com/hc/en-us/articles/201573102-Upgrading-the-Boundary-Meter).
+
+### Plugin Setup
+
+None
+
+### Plugin Configuration Fields
+
+None
+
+### Metrics Collected
+
+|Metric Name             |Description                                                   |
+|:-----------------------|:-------------------------------------------------------------|
+|WIN_SERVICE_KEEPALIVE   |A 1 or 0 value depending on the state of the service          |
+
+### Dashboards
+
+None
+
+### References
+
+None
---
+++ init.lua
@@ -0,0 +1,71 @@
+local os = require('os')
+local io = require('io')
+local json = require('json')
+local timer = require('timer')
+
+--
+-- Read and parse the plugin file.
+--
+plugin_file = io.open('plugin.json', 'r')
+plugin_json = plugin_file:read("*all")
+plugin_info = json.parse(plugin_json)
+plugin_file:close()
+
+--
+--  Read the parameter file.  It will contain an array of items which define
+--  the counter metrics to collect.
+--
+param_file = io.open('param.json', 'r')
+params_json = param_file:read("*all")
+params = json.parse(params_json)
+param_file:close()
+
+--
+-- Loop through each metric and store the parameters.  Currently supported are
+-- counter= The counter path
+-- factor=  The multiplication factor for the counter value.
+--
+
+local services = {}
+for _,item in pairs(params.items) do
+	services[item.service] = {down_cmd = item.down_cmd, event_on_stop = item.event_on_stop, event_on_restart = item.event_on_restart, interval=item.interval}
+end
+
+--
+-- Create the .ini file that the meter will parse.
+-- The filename needs to be plugin_services.ini
+-- Creating the .ini file is the purpose of the plugin.
+-- What ever after this is to make sure that the plugin does not exit.
+--
+file = io.open("plugin_services.ini", "w")
+
+for k, v in pairs(services) do
+	file:write("[".. k .. "]\n")
+	if (v.down_cmd ~= nil) then
+	    file:write("  down_cmd=".. v.down_cmd.."\n")
+	end
+	if (v.event_on_stop ~= nil) then
+	    file:write("  event_on_stop=".. v.event_on_stop.."\n")
+	end
+	if (v.event_on_restart ~= nil) then
+	    file:write("  event_on_restart=".. v.event_on_restart.."\n")
+	end
+	if (v.interval ~= nil) then
+	    file:write("  interval_sec=".. v.interval.."\n")
+	end
+end
+file:close()
+
+--Plugin start event.
+print ("_bevent:"..plugin_info.name.." version " .. plugin_info.version .. " Started|t:info")
+
+--Dummy function to be called after every polling interval.
+--
+function dummy()
+	--Do Nothing.
+end
+
+--Setting the polling interval. This will make sure that the dummy function is called after the specified
+--interval is expired. This is to make sure that the plugin does not exit. If the plugin exits
+--the meter will restart the plugin.
+timer.setInterval(300000, dummy)
